bed8c4c7db2d885247c7ae311835f3dd89a11b6c
Mark <markantonik@icloud.com>
2023-07-17 16:00:55 +0300

    добавил модель комментраия и лайка

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,31 @@ User = get_user_model()
 
 class News(models.Model):
     title = models.CharField(max_length=90, help_text='Название')
-    slug = models.SlugField(max_length=255, unique=True)
     text = models.TextField(help_text='Текст новости')
-    time_create = models.DateTimeField(auto_now_add=True, help_text='Дата создания новости')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_create = models.DateTimeField(auto_now_add=True, help_text='Время создания новости')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, help_text='Автор новости')
+
+    class Meta:
+        verbose_name = 'Новость'
 
     def __str__(self):
         return self.title
 
 
+class Comment(models.Model):
+    news = models.ForeignKey('News', on_delete=models.CASCADE, help_text='Новость на которую оставили комментарий')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, help_text='Автор комментария')
+    text = models.TextField(max_length=450, help_text='Текст комментария')
+    time_create = models.DateTimeField(auto_now_add=True, help_text='Время создания комментария')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+
+
+class Like(models.Model):
+    news = models.ForeignKey('News', on_delete=models.CASCADE, help_text='Новость на котрую поставили лайк')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, help_text='Автор лайка')
+    time_create = models.DateTimeField(auto_now_add=True, help_text='Время создания лайка')
+
+    class Meta:
+        verbose_name = 'Лайк'
